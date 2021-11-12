ca68344cc324f184ebc5513d403288907c299842
Liu Yang <zation1@gmail.com>
2021-11-13 06:36:17 +0800

    sorter

--- src/modules/skill/components/output-response-sorter.js
+++ src/modules/skill/components/output-response-sorter.js
@@ -1,56 +1,24 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import {
-  DndContext,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-} from '@dnd-kit/core';
-import {
-  arrayMove,
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
-  useSortable,
-} from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
-import { func, string, array, bool, node } from 'prop-types';
-import { Modal, Button } from 'antd';
-import { map, findIndex, propEq } from 'lodash/fp';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { func, string, array, bool } from 'prop-types';
+import { Modal } from 'antd';
+import { map } from 'lodash/fp';
 import useStyles from 'isomorphic-style-loader/useStyles';
+import { arrayMoveImmutable } from 'shared/utils/helper';
 
 import s from './output-response-sorter.less';
 
-const SortableItem = ({
-  id,
-  children,
-}) => {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-  } = useSortable({ id });
+const mapWithIndex = map.convert({ cap: false });
 
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
+const SortableItem = SortableElement(({ cnames }) => <div className={s.Item}>{cnames}</div>);
 
-  return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <div className={s.Item} ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      {children}
-    </div>
-  );
-};
-
-SortableItem.propTypes = {
-  id: string.isRequired,
-  children: node,
-};
+const SortableList = SortableContainer(({ items }) => (
+  <div>
+    {mapWithIndex(({ cId, cnames }, index) => (
+      <SortableItem key={cId} index={index} cnames={cnames} />
+    ))(items)}
+  </div>
+));
 
 const result = ({
   visible,
@@ -65,22 +33,9 @@ const result = ({
   useEffect(() => {
     setFinalValue(value || []);
   }, value);
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    }),
-  );
-  const handleDragEnd = useCallback((event) => {
-    const { active, over } = event;
-
-    if (active.id !== over.id) {
-      setFinalValue((items) => {
-        const oldIndex = findIndex(propEq('cId', active.id))(items);
-        const newIndex = findIndex(propEq('cId', over.id))(items);
-
-        return arrayMove(items, oldIndex, newIndex);
-      });
+  const onSortEnd = useCallback(({ oldIndex, newIndex }) => {
+    if (oldIndex !== newIndex) {
+      setFinalValue((items) => arrayMoveImmutable(items, oldIndex, newIndex));
     }
   }, [setFinalValue]);
 
@@ -88,24 +43,10 @@ const result = ({
     <Modal
       title={title}
       visible={visible}
-      onClose={onClose}
+      onCancel={onClose}
+      onOk={() => onChange(finalValue)}
     >
-      <DndContext
-        sensors={sensors}
-        collisionDetection={closestCenter}
-        onDragEnd={handleDragEnd}
-      >
-        <SortableContext items={finalValue} strategy={verticalListSortingStrategy}>
-          {map(({ cId, cnames }) => (
-            <SortableItem key={cId} id={cId}>
-              {cnames}
-            </SortableItem>
-          ))(finalValue)}
-        </SortableContext>
-      </DndContext>
-
-      <Button onClick={onClose}>取消</Button>
-      <Button onClick={onChange} type="primary">提交</Button>
+      <SortableList helperClass={s.List} items={finalValue} onSortEnd={onSortEnd} />
     </Modal>
   );
 };
--- src/shared/utils/helper.js
+++ src/shared/utils/helper.js
@@ -6,4 +6,19 @@ export const getCName = flow(
   join('&'),
 );
 
-export const a = 1;
+export const arrayMoveMutable = (array, fromIndex, toIndex) => {
+  const startIndex = fromIndex < 0 ? array.length + fromIndex : fromIndex;
+
+  if (startIndex >= 0 && startIndex < array.length) {
+    const endIndex = toIndex < 0 ? array.length + toIndex : toIndex;
+
+    const [item] = array.splice(fromIndex, 1);
+    array.splice(endIndex, 0, item);
+  }
+};
+
+export const arrayMoveImmutable = (array, fromIndex, toIndex) => {
+  const result = [...array];
+  arrayMoveMutable(result, fromIndex, toIndex);
+  return result;
+};
