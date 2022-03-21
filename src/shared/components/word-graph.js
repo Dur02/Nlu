75e24dd32e0de3c937fe64346996d385bc0a5000
Liu Yang <zation1@gmail.com>
2022-03-21 22:06:50 +0800

    add document

--- src/shared/components/word-graph.js
+++ src/shared/components/word-graph.js
@@ -1,12 +1,12 @@
 import {
   string,
 } from 'prop-types';
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { readWordGraph as readWordGraphAction } from 'shared/actions/skill';
 import { Input, message } from 'antd';
 import { useDispatch } from 'react-redux';
 import useStyles from 'isomorphic-style-loader/useStyles';
-import { map, values, flatten, flow, prop, nth, filter, isObject } from 'lodash/fp';
+import { map, values, flatten, flow, prop, nth, isObject } from 'lodash/fp';
 import { volcano, orange, gold, yellow, lime, green, cyan, blue, purple, magenta } from '@ant-design/colors';
 import classNames from 'classnames';
 import s from './word-graph.less';
@@ -24,18 +24,13 @@ const getLightColor = (index) => flow(
   nth(index % presetColors.length),
 )(presetColors);
 
-const createLine = (textIndex, nodeIndex) => new global.LeaderLine(
-  document.getElementById(`text-underline-${textIndex}`),
-  document.getElementById(`node-${nodeIndex}`),
-  { color: getColor(nodeIndex), size: 2, startSocket: 'bottom', path: 'magnet' },
-);
-
 const result = ({
   skillCode,
 }) => {
   useStyles(s);
   const [input, setInput] = useState('');
   const [activeNodeIndex, setActiveNodeIndex] = useState();
+  const [activeDocumentIndex, setActiveDocumentIndex] = useState();
   const [activeTextIndex, setActiveTextIndex] = useState();
   const [response, setResponse] = useState();
   const [loading, setLoading] = useState(false);
@@ -45,27 +40,27 @@ const result = ({
       message.error('请输入内容');
       return;
     }
-    if (input && response && response.input === input) {
+    if (input && prop(['wordGraph', 'input'])(response) === input) {
       message.warn('内容未变化');
       return;
     }
     if (input) {
       setLoading(true);
       const data = await dispatch(readWordGraphAction({ input, skillCode }));
-      setResponse(data[skillCode]);
+      setResponse(data);
       setLoading(false);
     }
-  }, [dispatch, input, response && response.input]);
+  }, [dispatch, input, prop(['wordGraph', 'input'])(response)]);
   const onChange = useCallback(({ target }) => setInput(target.value), [setInput]);
   const nodes = flow(
-    prop('dictNodes'),
+    prop(['wordGraph', 'dictNodes']),
     values,
     flatten,
     mapWithIndex((item, index) => ({ ...item, index })),
   )(response);
-  const activeNode = nodes[activeNodeIndex];
-
-  const lines = useRef();
+  const activeNode = nodes && nodes[activeNodeIndex];
+  const documents = prop('documents')(response);
+  const activeDocument = documents && documents[activeDocumentIndex];
 
   return (
     <div>
@@ -81,57 +76,50 @@ const result = ({
         <div
           className={classNames(s.Container, {
             [s.textEntered]: activeTextIndex >= 0,
-            [s.nodeEntered]: activeNodeIndex >= 0,
+            [s.tagEntered]: activeNodeIndex >= 0 || activeDocumentIndex >= 0,
           })}
         >
           <div className={s.Input}>
-            {mapWithIndex((text, index) => (
-              <span
-                className={classNames(s.Text, {
-                  [s.activeText]: activeTextIndex === index
-                  || (activeNode
-                    && index >= activeNode.pos
-                    && index < activeNode.pos + activeNode.length),
-                })}
-                onMouseEnter={() => {
-                  lines.current = flow(
-                    filter(({ pos, length }) => index >= pos && index < pos + length),
-                    map((node) => createLine(index, node.index)),
-                  )(nodes);
-                  setActiveTextIndex(index);
-                }}
-                onMouseLeave={() => {
-                  lines.current.forEach((line) => line.remove());
-                  setActiveTextIndex(undefined);
-                }}
-              >
-                {text}
+            {flow(
+              prop(['wordGraph', 'input']),
+              mapWithIndex((text, index) => (
                 <span
-                  id={`text-underline-${index}`}
-                  className={s.Underline}
-                  style={{ backgroundColor: getColor(activeNodeIndex) }}
-                />
-              </span>
-            ))(response.input)}
+                  key={index}
+                  className={classNames(s.Text, {
+                    [s.activeText]: activeTextIndex === index
+                    || (activeNode
+                      && index >= activeNode.pos
+                      && index < activeNode.pos + activeNode.length)
+                    || (activeDocument
+                      && index >= activeDocument.start
+                      && index < activeDocument.end),
+                  })}
+                  onMouseEnter={() => setActiveTextIndex(index)}
+                  onMouseLeave={() => setActiveTextIndex(undefined)}
+                >
+                  {text}
+                  <span
+                    className={s.Underline}
+                    style={{
+                      backgroundColor: getColor(activeNode ? activeNodeIndex : activeDocumentIndex),
+                    }}
+                  />
+                </span>
+              )),
+            )(response)}
           </div>
-          <div className={s.Nodes}>
+          <div className={s.Title}>词图：</div>
+          <div className={s.Tags}>
             {map(({ dictName, value, index, pos, length }) => (
               <div
-                className={classNames(s.Node, {
-                  [s.activeNode]: activeNodeIndex === index
+                key={index}
+                className={classNames(s.Tag, {
+                  [s.activeTag]: activeNodeIndex === index
                   || (activeTextIndex >= pos && activeTextIndex < pos + length),
                 })}
-                key={index}
-                id={`node-${index}`}
                 style={{ backgroundColor: getLightColor(index), borderColor: getColor(index) }}
-                onMouseEnter={() => {
-                  lines.current = [createLine(pos, index)];
-                  setActiveNodeIndex(index);
-                }}
-                onMouseLeave={() => {
-                  lines.current.forEach((line) => line.remove());
-                  setActiveNodeIndex(undefined);
-                }}
+                onMouseEnter={() => setActiveNodeIndex(index)}
+                onMouseLeave={() => setActiveNodeIndex(undefined)}
               >
                 <div>词典名：{dictName}</div>
                 <div>值：{isObject(value) ? value.value : value}</div>
@@ -139,6 +127,26 @@ const result = ({
             ))(nodes)}
           </div>
           {nodes.length === 0 && <div className={s.Empty}>未解析出词图</div>}
+
+          <div className={s.Title}>文档：</div>
+          <div className={s.Tags}>
+            {mapWithIndex(({ doc: { content }, weight, start, end }, index) => (
+              <div
+                key={index}
+                className={classNames(s.Tag, {
+                  [s.activeTag]: activeDocumentIndex === index
+                  || (activeTextIndex >= start && activeTextIndex < end),
+                })}
+                style={{ backgroundColor: getLightColor(index), borderColor: getColor(index) }}
+                onMouseEnter={() => setActiveDocumentIndex(index)}
+                onMouseLeave={() => setActiveDocumentIndex(undefined)}
+              >
+                <div>内容：{content}</div>
+                <div>权重：{weight}</div>
+              </div>
+            ))(documents)}
+            {documents.length === 0 && <div className={s.Empty}>未解析出文档</div>}
+          </div>
         </div>
       )}
     </div>
