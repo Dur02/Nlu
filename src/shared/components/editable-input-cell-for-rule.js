4ed4e1ae278a66713c7c931df67264eea57055ea
whatever\Dur02 <hwl3088@cannano.cn>
2022-11-24 17:25:13 +0800

    fix old rule mark missing slot name

--- src/shared/components/editable-input-cell-for-rule.js
+++ src/shared/components/editable-input-cell-for-rule.js
@@ -2,7 +2,7 @@
 import { string, func } from 'prop-types';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Input, Spin, Tooltip } from 'antd';
-import { flow, compact, map, split, flatten, difference, toString } from 'lodash/fp';
+import { flow, compact, map, split, flatten, difference, toString, includes, replace } from 'lodash/fp';
 import { WarningOutlined } from '@ant-design/icons';
 
 const result = ({
@@ -14,17 +14,33 @@ const result = ({
   // 为rule.js修改的editable-input-cell组件
 
   // 已添加说法的说法名经处理后去除#和|的数组
-  const splitRuleName = flow(
-    split('#'),
-    compact,
-    map((item) => split('｜')(item)),
-    flatten,
-    map((item) => split('|')(item)),
-    flatten,
-  )(value);
+  const getSplitRuleName = () => {
+    // 如果说法中有表示槽位的分隔符就进行处理,否则没有槽位直接返回slotNames
+    if ((includes('{')(value) && includes('}')(value)) || (includes('#')(value))) {
+      // 处理老说法中特有的以{}表示槽位,用##替代{},新说法也不会影响
+      const str = flow(
+        replace(/{/g, '#'),
+        replace(/}/g, '#'),
+      )(value);
+      // 用正则表达式截取所有#之间的值作为一个数组
+      const array = str.match(/#(.+?)#/g);
+      // 最终获得包含所有槽位的数组
+      return flow(
+        map((item) => (replace(/#/g, '')(item))),
+        // split('#'),
+        compact,
+        // 中文和英文的分隔符都要去除
+        map((item) => split('｜')(item)),
+        flatten,
+        map((item) => split('|')(item)),
+        flatten,
+      )(array);
+    }
+    return slotNames;
+  };
 
   // 与存在slots的名字进行对比确认是否有遗漏的语义槽未添加
-  const compareArray = difference(splitRuleName, slotNames);
+  const compareArray = difference(getSplitRuleName(), slotNames);
 
   const [editing, setEditing] = useState(false);
   const [submitting, setSubmitting] = useState(false);
